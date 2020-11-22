a794f1872cd49ffb928580d62a055d5b6b387ff1
gaoyi <gaoyia@qq.com>
2020-11-22 14:38:34 +0800

    修复代码错误

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const indicative = class AppBootHook {
     this.app = app;
   }
   async didLoad() {
-    require("indicative/validator").configure(app.config.indicative); // 加载配置
+    require("indicative/validator").configure(this.app.config.indicative); // 加载配置
     this.app.validateAll = require("indicative/validator").validateAll; //引入验证器ALL
     this.app.validate = require("indicative/validator").validate; //引入验证器
     this.app.validations = require("indicative/validator").validations;
     this.app.sanitize = require("indicative/sanitizer").sanitize; //引入过滤器
-    this.app.sanitizations.require("indicative/sanitizer").sanitize; //引入净化器
+    this.app.sanitizations = require("indicative/sanitizer").sanitize; //引入净化器
     // 引入validate目录，并注入app实例
     const directory = path.join(this.app.config.baseDir, "app/validate");
     this.app.loader.loadToApp(directory, "validate");
   }
 };
-module.exports = AppBootHook;
+module.exports = indicative;
